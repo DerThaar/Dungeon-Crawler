0fe92ad2d4171c9bb89e968dd4635ce2efdc5d28
Der_Thaar <wazap@gmx.de>
2018-08-30 13:58:50 +0200

    1

---
+++ Assets/_Scripts/MarkovNameGenerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MarkovNameGenerator
+{
+	Dictionary<string, List<char>> chains = new Dictionary<string, List<char>>();
+	List<string> sample = new List<string>();
+	List<string> used = new List<string>();
+	Random random = new Random();
+	int order;
+	int minLength;
+
+	public MarkovNameGenerator(IEnumerable<string> sampleNames, int order, int minLength)
+	{
+		//clamp parameters
+		if(order < 1) { order = 1; }
+		if(minLength < 1) { minLength = 1; }
+
+	}
+}
